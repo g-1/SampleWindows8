f759f23c97a98fdef573bdc0323cc74e13b94675
Takahiro Naka <naka.takahiro@gmail.com>
2013-02-18 10:44:30 +0900

    First Commit Sample

---
+++ MediaTest01/MediaTest01/MainPage.xaml.cpp
@@ -0,0 +1,77 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage クラスの実装。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace MediaTest01;
+
+using namespace concurrency;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	//初期化ここでええのん？
+	_rootFolder = Windows::Storage::KnownFolders::MusicLibrary;
+}
+
+/// <summary>
+/// このページがフレームに表示されるときに呼び出されます。
+/// </summary>
+/// <param name="e">このページにどのように到達したかを説明するイベント データ。Parameter 
+/// プロパティは、通常、ページを構成するために使用します。</param>
+void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(void) e;	// 未使用のパラメーター
+}
+
+
+void MediaTest01::MainPage::Play_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Play
+	using namespace Windows::Storage;
+	using namespace Windows::Storage::Streams;
+
+	auto getFileTask = create_task(_rootFolder->GetFileAsync("house_01.mp3"));
+	getFileTask.then([=](StorageFile^ soundFile) {       
+        //success
+		auto streamTask = create_task(soundFile->OpenReadAsync());
+		streamTask.then([=](IRandomAccessStream^ randomAccessStream){
+			if(randomAccessStream){
+				MediaElement1->SetSource(randomAccessStream, soundFile->ContentType);
+			}
+		});
+
+    });
+
+
+}
+
+
+void MediaTest01::MainPage::Stop_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	//Stop
+	MediaElement1->Stop();
+
+}
+
+
+void MediaTest01::MainPage::Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+
+}
---
+++ MediaTest01/MediaTest01/MainPage.xaml.h
@@ -0,0 +1,29 @@
+﻿//
+// MainPage.xaml.h
+// MainPage クラスの宣言。
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace MediaTest01
+{
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+	public ref class MainPage sealed
+	{
+		Windows::Storage::StorageFolder^ _rootFolder;
+
+	public:
+		MainPage();
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+	private:
+		void Play_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Stop_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
